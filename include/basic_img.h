42cb739ec1abb86612d7f8509eacdf26efc57f36
MagicGirl <1107480959@qq.com>
2017-12-07 19:15:07 +0800

    add contrast and brightness fuctions

--- include/basic_img.h
+++ include/basic_img.h
@@ -20,4 +20,11 @@ bool IPP_split(version v,channel c);
 //分离RGB值，可选参数为RED,GREEN,BLUE
 
 
+// 改变图片的亮度，brightness表示增加的亮度(可正可负)
+bool IPP_brightness(version v, uchar brightness);
+// 改变图片的对比度，contrast_rate表示对比度的相对比率值(>0)
+bool IPP_contrast(version v, double contrast_rate);
+
+
+
 #endif //IPP_ZOOM_H
